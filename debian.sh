88fe593fbbf21b3dcf11a02e47f986d44f443d31
mosesprime <huntermoses007@gmail.com>
2023-05-20 13:36:57 -0400

    clean up alacritty script

--- debian.sh
+++ debian.sh
@@ -27,19 +27,5 @@ hx --grammar fetch
 hx --grammar build
 cd ~
 
-# https://github.com/alacritty/alacritty/blob/master/INSTALL.md
-echo "-- Install Alacritty Terminal --"
-sudo apt install cmake gzip pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-cd GitRepos
-git clone https://github.com/alacritty/alacritty.git
-cd alacritty
-cargo build --release # supports Wayland and X11
-sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-# ADD desktop entry ???
-sudo mkdir -p /usr/local/share/man/man1
-gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
-# ADD shell completions ???
-
 echo "-- Cleaning Up --"
 sudo apt autoremove -y
