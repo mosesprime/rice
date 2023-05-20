2f81959f45ceabd376a6c0258a8bd7f06b9f4376
mosesprime <huntermoses007@gmail.com>
2023-05-20 14:02:00 -0400

    cleaned up

--- alacritty/debian.sh
+++ alacritty/debian.sh
@@ -4,15 +4,17 @@ sudo apt install cmake gzip pkg-config libfreetype6-dev libfontconfig1-dev libxc
 git clone https://github.com/alacritty/alacritty.git
 cd alacritty
 cargo build --release
-# install terminfo globally ??
+# install terminfo globally
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-# ADD desktop entry ??
-sudo cp target/release/alacritty /usr/local/bin
-sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-#sudo desktop-file-install extra/linux/Alacritty.desktop
-#sudo update-desktop-database
+# TODO: desktop entry
+# sudo cp target/release/alacritty /usr/local/bin
+# sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+# sudo desktop-file-install extra/linux/Alacritty.desktop
+# sudo update-desktop-database
 # install man page
 sudo mkdir -p /usr/local/share/man/man1
 gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
 gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
-# ADD shell completeions ??
+# TODO: shell completeions
+cd ..
+rm -rf alacritty
