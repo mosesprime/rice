e95e73f94b54240eb229071a481dc92f193bafbd
mosesprime <huntermoses007@gmail.com>
2023-05-20 14:10:23 -0400

    added neovim to start script

--- debian.sh
+++ debian.sh
@@ -4,17 +4,21 @@
 
 echo "-- Initialize Automatic Setup & Ricing --"
 cd ~
-mkdir GitRepos
+mkdir TEMP
+cd TEMP
 
 echo "-- SetUp Packages --"
 sudo apt update && sudo apt upgrade -y
-sudo apt install git build-essential python3 neofetch htop curl gzip
+sudo apt install git build-essential python3 neofetch htop curl gzip cmake
 
 # https://www.rust-lang.org/tools/install
 echo "-- Install Rust Toolchains --"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- --y
 rustup target add wasm32-unknown-unknown wasm32-wasi
 rustup component add rust-analyzer
+
+# install & configure NeoVim
+sh ./neovim/debian.sh
 
 echo "-- Cleaning Up --"
 sudo apt autoremove -y
